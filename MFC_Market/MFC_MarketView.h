761e6161df6c3d5d641f9bac8128843339e2bb81
志伟 张 <1009535916@qq.com>
2019-06-10 12:28:20 +0000

    Added 18 files to MFC_Market

---
+++ MFC_Market/MFC_MarketView.h
@@ -0,0 +1,49 @@
+﻿
+// MFC_MarketView.h: CMFCMarketView 类的接口
+//
+
+#pragma once
+
+
+class CMFCMarketView : public CView
+{
+protected: // 仅从序列化创建
+	CMFCMarketView() noexcept;
+	DECLARE_DYNCREATE(CMFCMarketView)
+
+// 特性
+public:
+	CMFCMarketDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFCMarketView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFC_MarketView.cpp 中的调试版本
+inline CMFCMarketDoc* CMFCMarketView::GetDocument() const
+   { return reinterpret_cast<CMFCMarketDoc*>(m_pDocument); }
+#endif
+
---
+++ MFC_Market/Resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCMarket.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCMarketTYPE               130
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDI_APERTURE                    311
+#define IDI_EDIT                        312
+#define IDI_ERR                         313
+#define IDI_LOGIN                       314
+#define IDI_LOGOUT                      315
+#define IDI_MINUS                       316
+#define IDI_ADD                         317
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        318
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
