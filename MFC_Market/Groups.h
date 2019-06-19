eeb06df290f8e851dd15681b55bbb4f2562fff5b
SkyNetX007 <1187116482@qq.com>
2019-06-20 01:14:49 +0800

    access reupload

---
+++ MFC_Market/Groups.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <afx.h>
+#include <list>
+
+#define _ACCOUNT_LIST_FILE "../etc/passwd"
+
+using namespace std;
+
+class ACCESS
+{
+	friend class UsersList;
+
+public:
+	int UID = 0;
+	CString ACCOUNT = TEXT("\0");
+	CString COMMENT = TEXT("\0");
+	CString GroupType = TEXT("\0");
+	string PASSWORD_MD5 = 0;
+
+	void Edit(int _UID, CString _ACCOUNT, CString _COMMENT, CString _GroupType = TEXT("guest"), string _PASSWORD_MD5 = 0);
+};
+
+class UsersList
+{
+	list<ACCESS> content;
+	int length = 0;
+
+public:
+	void ReadFile();
+	void WriteFile();
+
+	list<ACCESS>::iterator getFirstUser() { return content.begin(); }
+	list<ACCESS>::iterator getLastUser() { return --content.end(); }
+	list<ACCESS>::iterator Find(CString _Username);
+	void Sort(CString mode);
+	void Delete(list<ACCESS>::iterator _Account);
+	void Add(int _UID, CString _ACCOUNT, CString _COMMENT, CString _GroupType = TEXT("guest"), string _PASSWORD_MD5 = 0);
+	int GetLength();
+};
